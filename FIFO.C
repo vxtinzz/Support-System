2fefed613f652993c1738f6d567abae1abf097d5
vxtinzz <vitin.ti679@gmail.com>
2025-10-24 19:12:53 -0300

    Add files via upload

--- FIFO.C
+++ FIFO.C
@@ -36,7 +36,6 @@ void AdicionarTicket(Fila *f)
         while (fscanf(arq, "%d;%d;%199[^;];%d\n",
                       &newTicket->id, &newTicket->idSolicitante, newTicket->descricao, &newTicket->prioridade) != EOF)
         {
-            printf("%d;%d;%199[^;];%d\n", newTicket->id, newTicket->idSolicitante, &newTicket->descricao, &newTicket->prioridade);
             lastId = newTicket->id;
         }
         fclose(arq);
@@ -75,6 +74,7 @@ void AdicionarTicket(Fila *f)
         printf("Erro ao abrir o arquivo para escrita.\n");
         return;
     }
+    fprintf(arq, "%d;%d;%s;%d\n", newTicket->id, newTicket->idSolicitante, newTicket->descricao, newTicket->prioridade);
     fclose(arq);
     printf("Ticket #%d adicionado com sucesso!\n", newTicket->id);
 }
@@ -122,10 +122,80 @@ void READ(int id)
             printf("Prioridade: %d\n", Ticket.prioridade);
             printf("------------------------\n");
         }
+        else
+        {
+            printf("ID nao encontrado na base de dados");
+            printf("\n------------------------\n");
+            return;
+        }
     }
     fclose(arq);
 }
 
+void AtenderTicket()
+{
+    // declaration
+    FILE *arq;
+    FILE *temp;
+    arq = fopen("../data/tickets.txt", "r");
+    temp = fopen("../data/temp.txt", "a");
+    Fila *f = (Fila *)malloc(sizeof(Fila));
+    Ticket *bigPriority = (Ticket *)malloc(sizeof(Ticket));
+    Ticket *ticket = (Ticket *)malloc(sizeof(Ticket));
+    int id, idSolicitante, prioridade;
+    char descricao[200];
+
+    // init
+    bigPriority->prioridade = 0;
+    f->begin = NULL;
+    f->end = NULL;
+    f->tot = 0;
+
+    if (arq != NULL)
+    {
+        while (fscanf(arq, "%d;%d;%199[^;];%d\n", &ticket->id, &ticket->idSolicitante, ticket->descricao, &ticket->prioridade) != EOF)
+        {
+            Ticket *loadTicket = (Ticket *)malloc(sizeof(Ticket));
+            loadTicket = ticket;
+            loadTicket->next = NULL;
+            if (f->end == NULL)
+            {
+                f->begin = loadTicket;
+            }
+            else
+            {
+                f->end->next = loadTicket;
+            }
+            f->end = loadTicket;
+            f->tot++;
+            if (loadTicket->prioridade > bigPriority->prioridade)
+            {
+                bigPriority = loadTicket;
+            }
+        }
+
+        Ticket *aux = f->begin;
+        while (aux != NULL)
+        {
+            if (aux->id != bigPriority->id)
+            {
+                fprintf(temp, "%d;%d;%s;%d\n", aux->id, aux->idSolicitante, aux->descricao, aux->prioridade);
+            }
+            aux = aux->next;
+        }
+        fclose(arq);
+        fclose(temp);
+
+        remove("../data/tickets.txt");
+        rename("../data/temp.txt", "../data/tickets.txt");
+
+        printf("Ticket Atendido Com Sucesso!!\n");
+    }
+}
+
+void AtenderTicketById(int id)
+{
+}
 int main()
 {
     int op, id;
@@ -146,6 +216,24 @@ int main()
             break;
         case 2:
             system("cls");
+            do
+            {
+                printf("1-Atender Ticket Pelo ID\n2-Atender Ticket Mais Urgente\nEscolha sua opcao: ");
+                scanf("%d", &op);
+
+                switch (op)
+                {
+                case 1:
+                    system("cls");
+                    printf("Busca por ID: ");
+                    scanf("%d", &id);
+                    AtenderTicketById(id);
+                    break;
+                case 2:
+                    system("cls");
+                    AtenderTicket();
+                }
+            } while (op != 1 || op != 2);
             break;
         case 3:
             system("cls");
