cffda3d13e37b639e066067c6f8c989b50d67732
vxtinzz <vitin.ti679@gmail.com>
2025-10-24 12:48:44 -0300

    starting FILE
    starting file and raw functions, later it will be refactored to separate functions and good practices

---
+++ FIFO.C
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Ticket
+{
+    int id;
+    int idSolicitante;
+    char descricao[200];
+    int prioridade;
+    Ticket *next;
+} Ticket;
+
+typedef struct
+{
+    Ticket *begin;
+    Ticket *end;
+    int tot;
+} Fila;
+
+void AdicionarTicket(Fila *f)
+{
+    Ticket *newTicket = (Ticket *)malloc(sizeof(Ticket));
+    if (!newTicket)
+    {
+        printf("Erro ao alocar memória.\n");
+        return;
+    }
+
+    int lastId = 0;
+    FILE *arq;
+    arq = fopen("../data/tickets.txt", "r");
+
+    if (arq != NULL)
+    {
+        while (fscanf(arq, "%d;%d;%199[^;];%d\n",
+                      &newTicket->id, &newTicket->idSolicitante, newTicket->descricao, &newTicket->prioridade) != EOF)
+        {
+            printf("%d;%d;%199[^;];%d\n", newTicket->id, newTicket->idSolicitante, &newTicket->descricao, &newTicket->prioridade);
+            lastId = newTicket->id;
+        }
+        fclose(arq);
+    }
+
+    newTicket->id = lastId + 1;
+    printf("ID do Solicitante: ");
+    scanf("%d", &newTicket->idSolicitante);
+    getchar();
+
+    printf("Descreva o Problema:\n ");
+    fgets(newTicket->descricao, 200, stdin);
+    newTicket->descricao[strcspn(newTicket->descricao, "\n")] = '\0';
+
+    do
+    {
+        printf("Prioridade (1-5):");
+        scanf("%d", &newTicket->prioridade);
+    } while (newTicket->prioridade < 1 || newTicket->prioridade > 5);
+
+    newTicket->next = NULL;
+    if (f->end == NULL)
+    {
+        f->begin = newTicket;
+    }
+    else
+    {
+        f->end->next = newTicket;
+    }
+    f->end = newTicket;
+    f->tot++;
+
+    arq = fopen("../data/tickets.txt", "a");
+    if (arq == NULL)
+    {
+        printf("Erro ao abrir o arquivo para escrita.\n");
+        return;
+    }
+    fclose(arq);
+    printf("Ticket #%d adicionado com sucesso!\n", newTicket->id);
+}
+
+void READALL()
+{
+    Ticket newTicket;
+    FILE *arq;
+    arq = fopen("../data/tickets.txt", "r");
+    if (arq == NULL)
+    {
+        printf("Erro ao abrir o arquivo!\n");
+        return;
+    }
+    printf("----Lista de Tickets----\n");
+    while (fscanf(arq, "%d;%d;%199[^;];%d\n", &newTicket.id, &newTicket.idSolicitante, newTicket.descricao, &newTicket.prioridade) != EOF)
+    {
+        printf("ID: %d\n", newTicket.id);
+        printf("ID Solicitante: %d\n", newTicket.idSolicitante);
+        printf("Descricao: %s\n", newTicket.descricao);
+        printf("Prioridade: %d\n", newTicket.prioridade);
+        printf("------------------------\n");
+    }
+    fclose(arq);
+}
+
+void READ(int id)
+{
+    Ticket Ticket;
+    FILE *arq;
+    arq = fopen("../data/tickets.txt", "r");
+    if (arq == NULL)
+    {
+        printf("Erro ao abrir o arquivo!\n");
+        return;
+    }
+    printf("------------------------\n");
+    while (fscanf(arq, "%d;%d;%199[^;];%d\n", &Ticket.id, &Ticket.idSolicitante, Ticket.descricao, &Ticket.prioridade) != EOF)
+    {
+        if (id == Ticket.id)
+        {
+            printf("ID: %d\n", Ticket.id);
+            printf("ID Solicitante: %d\n", Ticket.idSolicitante);
+            printf("Descricao: %s\n", Ticket.descricao);
+            printf("Prioridade: %d\n", Ticket.prioridade);
+            printf("------------------------\n");
+        }
+    }
+    fclose(arq);
+}
+
+int main()
+{
+    int op, id;
+    Fila fila = {NULL, NULL, 0};
+
+    printf("==Sistema de Suporte==\n");
+    do
+    {
+        printf("\n1 - Adicionar Ticket\n2 - Atender Ticket\n3 - Exibir Tickets\n4 - Buscar Ticket por ID\n5 - SIMULACAO[10]\n Escolha sua opcao: ");
+        scanf("%d", &op);
+        switch (op)
+        {
+        case 0:
+            break;
+        case 1:
+            system("cls");
+            AdicionarTicket(&fila);
+            break;
+        case 2:
+            system("cls");
+            break;
+        case 3:
+            system("cls");
+            READALL();
+            break;
+        case 4:
+            system("cls");
+            printf("Busca por ID: ");
+            scanf("%d", &id);
+            READ(id);
+            break;
+        case 10:
+            break;
+        }
+    } while (op != 0);
+    return 0;
+}
